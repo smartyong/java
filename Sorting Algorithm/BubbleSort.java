fb584a5733cb562d66d23663e535e80e2f3d19a5
smartyong <315274305@qq.com>
2019-01-31 13:49:14 +0800

    Update BubbleSort.java
    添加代码注释

--- Sorting Algorithm/BubbleSort.java
+++ Sorting Algorithm/BubbleSort.java
@@ -1,5 +1,10 @@
-package com.shanghaionstar.dealerinfo.bean;
-
+package com.sandy;
+/**
+冒泡排序（假如从小到大排序）
+第一个元素和第二个元素比较大小，如果第一个元素比第二个大，则交换值，
+然后第二个元素再和第三个元素比较大小，如果第二个元素比第三个大，则交换值，。。。以此类推
+第一遍最大值找到 在末尾，第二遍找到第二大值 在倒数第二位置。。。。以此类推  最终排序完成
+*/
 public class BubbleSort {
 
 	public static void main(String[] args) {
